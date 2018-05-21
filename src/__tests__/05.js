6e02e2728200c4f1e56e9746ede0840a042a573b
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-05-21 15:20:36 -0400

    complete 05 - prop collections

--- src/__tests__/05.js
+++ src/__tests__/05.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {renderToggle, Simulate} from '../../test/utils'
-import Usage from '../exercises-final/05'
-// import Usage from '../exercises/05'
+// import Usage from '../exercises-final/05'
+import Usage from '../exercises/05'
 
 test('renders a toggle component', () => {
   const handleToggle = jest.fn()
@@ -36,8 +36,11 @@ test('can also toggle with the custom button', () => {
 /*
 http://ws.kcd.im/?ws=react%20patterns&e=05&em=andreiidobrinski@gmail.com
 */
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
+test('I submitted my elaboration and feedback', () => {
+  const submitted = true // change this when you've submitted!
   expect(submitted).toBe(true)
 })
 ////////////////////////////////
+// Prop collections are useful when you need to spread several props
+// over a component.
+// However, prop getters seem like the more useful and robust pattern.
